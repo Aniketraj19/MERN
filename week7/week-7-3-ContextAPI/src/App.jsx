451ae420e5c541d285f20dc40ecc414b91d62fa7
Aniketraj19 <aniketraj19verma@gmail.com>
2025-04-06 19:46:26 +0530

    context API

---
+++ week7/week-7-3-ContextAPI/src/App.jsx
@@ -0,0 +1,55 @@
+// Prop Drilling
+
+import { useContext, useState } from "react";
+import { CountContext } from "./context";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    // wrap any component that wants to use the context api in context.provider to teleport the count variable directly
+    <div>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
+    </div>
+  );
+}
+
+function Count({ setCount }) {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons setCount={setCount} />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return <div>{count}</div>;
+}
+
+function Buttons({ setCount }) {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
